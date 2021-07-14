28e430df882dd6e907d539d301f67954c87ccf5e
Anton Dovnar <fode4cun@gmail.com>
2021-07-14 15:53:13 +0300

    Good vs Evil

--- kyu6/good_vs_evil.py
+++ kyu6/good_vs_evil.py
@@ -1,6 +1,23 @@
 def goodVsEvil(good, evil):
-    good = sum(map(int, good.split()))
-    evil = sum(map(int, evil.split()))
+    good_score = {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 3,
+        5: 4,
+        6: 10
+    }
+    evil_score = {
+        1: 1,
+        2: 2,
+        3: 2,
+        4: 2,
+        5: 3,
+        6: 5,
+        7: 10
+    }
+    good = sum([good_score[key] * times for key, times in enumerate(map(int, good.split()), 1)])
+    evil = sum([evil_score[key] * times for key, times in enumerate(map(int, evil.split()), 1)])
 
     if good > evil:
         return "Battle Result: Good triumphs over Evil"
