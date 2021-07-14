ffd5b192525e211c1314e47873763d528f30eaec
Anton <fode4cun@gmail.com>
2021-07-15 01:55:25 +0300

    Product of consecutive Fib Numbers

--- kyu5/product_of_consecutive_fib.py
+++ kyu5/product_of_consecutive_fib.py
@@ -1,6 +1,9 @@
 def productFib(prod):
+    if prod == 0:
+        return [0, 1, True]
+
     cache = {0: 0, 1: 1}
-    for i in range(2, int((prod + 1) ** 0.5)):
+    for i in range(2, max(prod, 5)):
         cache[i] = cache[i-1] + cache[i-2]
         previous = cache[i-1]
         current = cache[i]
