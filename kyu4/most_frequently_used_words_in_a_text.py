33c6e56dd380a433b7889330e229d18b4591df56
Anton Dovnar <fode4cun@gmail.com>
2021-07-18 15:32:47 +0300

    Most frequently used words in a text

--- kyu4/most_frequently_used_words_in_a_text.py
+++ kyu4/most_frequently_used_words_in_a_text.py
@@ -1,15 +1,7 @@
 import re
 from collections import Counter
 
-def weight(line):
-    items = [
-        ord(c) if ord(c) > 96 else ord(c) + 100
-        for c in line
-    ]
-    return sum(items)
-
 
 def top_3_words(text):
-    text = re.findall(r"([\']*(?:[a-z]+[\']+[a-z]+|[a-z]+)[\']*)", text, re.I)
-    occurences = Counter(text).most_common()
-    return [character.lower() for character, _ in sorted(occurences, key=lambda x: (-x[1], weight(x[0])))][:3]
+    text = re.findall(r"'*[a-z][a-z']*", text.lower())
+    return [w for w, _ in Counter(text).most_common(3)]
